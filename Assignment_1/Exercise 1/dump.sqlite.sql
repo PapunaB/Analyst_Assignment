5915c03b7a128d7f158a4f006b21802bf6ccfe9d
PapunaB <84291143+PapunaB@users.noreply.github.com>
2023-05-26 18:38:58 +0400

    Add files via upload

---
+++ Assignment_1/Exercise 1/dump.sqlite.sql
@@ -0,0 +1,21 @@
+-- TABLE
+CREATE TABLE Exercise1 (
+    Branch varchar(255),
+    Geographical_Region varchar(255),
+    Narrow_Location int,
+    Population int,
+  	Customers2020 int,
+    Customers2021 int,
+    Customers2022 int,
+  	Employee_ID int,
+  	Customer_PA_2020 int,
+  	Customer_PA_2021 int,
+ 	Customer_PA_2022 int
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
